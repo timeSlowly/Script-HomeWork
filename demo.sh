7ca01a181fefe2c88f72d732b90219cce4f1354f
time <295380726@qq.com>
2017-10-21 03:19:56 +0800

    add demo.sh

---
+++ demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -d $1 ];then
+	echo 'error: dir exists'
+	exit
+else
+	mkdir $1
+	cd $1
+	mkdir css js
+	echo "h1{color: red;}" > css/style.css
+	echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+	echo -e "var string = "hello World"\nalert(string)" > js/main.js
+	exit
+fi
